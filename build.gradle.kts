87592ae49f2656bd0616f96023c800032c0d258d
Dr. Elden Wayne Whalen III, ShD <superyoopercomputers@protonmail.com>
2026-07-20 08:20:59 -0400

    Initialize build.gradle.kts for Ktor application

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    kotlin("jvm") version "2.0.0"
+    kotlin("plugin.serialization") version "2.0.0"
+    id("io.ktor.plugin") version "3.0.0"
+    application
+}
+
+group = "com.example"
+version = "1.0.0"
+
+application {
+    mainClass.set("com.example.ApplicationKt")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Ktor Core & Netty
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-netty-jvm")
+    
+    // Features & Middleware
+    implementation("io.ktor:ktor-server-content-negotiation-jvm")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
+    implementation("io.ktor:ktor-server-status-pages-jvm")
+    implementation("io.ktor:ktor-server-rate-limit-jvm")
+    implementation("io.ktor:ktor-server-cors-jvm")
+    implementation("io.ktor:ktor-server-default-headers-jvm")
+    
+    // Logging
+    implementation("ch.qos.logback:logback-classic:1.5.6")
+
+    testImplementation("io.ktor:ktor-server-test-host-jvm")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+}
